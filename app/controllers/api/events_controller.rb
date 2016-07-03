ec5d0808fe3f32b4c2fe9cd89c56f4aef7889e36
theXodus <jprice1990@gmail.com>
2016-07-02 23:29:42 -0500

    API Controller

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,37 @@
+class API::EventsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  before_filter :set_access_control_headers
+
+  def create
+    registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+
+    if registered_application.nil?
+      render json: "Unregistered application", status: :unprocessable_entity
+    end
+
+    @event = registered_application.events.build(event_params)
+
+    if @event.save
+      render json: @event, status: :created
+    else
+      render json: {errors: @event.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Orgin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
+
+  def preflight
+    head 200
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   resources :registered_applications
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   devise_for :users
   root to: "welcome#index"
 
